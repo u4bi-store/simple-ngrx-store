b66827be4f625fd341e664d5104268850d7e75cf
u4bi <rootcode10@gmail.com>
2017-10-22 00:32:16 +0900

    app component ngrx store api render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,38 @@ import { INCREMENT, DECREMENT, RESET } from './counter';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    counter: Observable<number>;
+
+    constructor(private store: Store<AppState>) {
+        
+        this.counter = store.select('counter');
+
+    }
+    
+    /* 증가 */
+    increment(){
+        this.store.dispatch({
+            type: INCREMENT
+        });
+
+    }
+    
+    /* 감소 */
+    decrement(){
+        this.store.dispatch({
+          type: DECREMENT
+        });
+
+    }
+    
+    /* 초기화 */
+    reset(){
+        this.store.dispatch({
+          type: RESET
+        });
+
+    }
+
 }
