1ff1a9ea86c708be0d1874b2375c3c87a3ad73d6
u4bi <rootcode10@gmail.com>
2017-10-22 04:56:16 +0900

    router-child component render
    - /router/child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RouterComponent } from './router/router.component';
+import { RouterChildComponent } from './router/router-child/router-child.component';
 
 const routes: Routes = [
   {
     path: 'router',
-    component: RouterComponent
+    component: RouterComponent,
+    children: [
+      {
+        path : 'child',
+        component : RouterChildComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
   declarations: [
-    RouterComponent
+    RouterComponent,
+    RouterChildComponent
   ],
   imports: [
     RouterModule.forRoot(routes)
