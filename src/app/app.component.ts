9e3f57c0da7cd437050f3c8485eb5ba2316e1b83
u4bi <rootcode10@gmail.com>
2017-10-22 00:27:50 +0900

    app component import counter reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AppState } from './app-state';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { INCREMENT, DECREMENT, RESET } from './counter';
 
 @Component({
   selector: 'app-root',
